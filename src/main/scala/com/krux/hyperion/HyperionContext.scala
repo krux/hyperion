cc2b72bd3f2a2e93c1726d742fe099da67538459
Seth Yates <seth.yates@krux.com>
2015-07-02 09:20:15 -0700

    (Fixes #68) Refactor out aws keys in config

--- src/main/scala/com/krux/hyperion/HyperionContext.scala
+++ src/main/scala/com/krux/hyperion/HyperionContext.scala
@@ -14,23 +14,29 @@ class HyperionContext(config: Config) {
   lazy val logUri = config.getString("hyperion.log.uri")
 
   lazy val failureRerunMode = config.getString("hyperion.failure_rerun_mode")
-
-  // These should be moved to EC2 and EMR sections
-  lazy val keyPair = Try(config.getString("hyperion.aws.keypair")).toOption
-  lazy val region = config.getString("hyperion.aws.region")
-  lazy val availabilityZone = Try(config.getString("hyperion.aws.availability_zone")).toOption
-  lazy val subnetId = Try(config.getString("hyperion.aws.subnet")).toOption
   lazy val role = config.getString("hyperion.role")
   lazy val resourceRole = config.getString("hyperion.resource.role")
 
-  // These should be removed and made an application concern
-  lazy val accessKeyId = config.getString("hyperion.aws.access_key_id")
-  lazy val accessKeySecret = config.getString("hyperion.aws.access_key_secret")
+  // EC2 default configuration
+  lazy val ec2Region = config.getString("hyperion.aws.ec2.region")
+  lazy val ec2KeyPair = Try(config.getString("hyperion.aws.ec2.keypair")).toOption
+  lazy val ec2AvailabilityZone = Try(config.getString("hyperion.aws.ec2.availability_zone")).toOption
+  lazy val ec2SubnetId = Try(config.getString("hyperion.aws.ec2.subnet")).toOption
+  lazy val ec2Role = config.getString("hyperion.aws.ec2.role")
+  lazy val ec2ResourceRole = config.getString("hyperion.aws.ec2.resource.role")
 
   lazy val ec2SecurityGroup = config.getString("hyperion.aws.ec2.securitygroup")
   lazy val ec2InstanceType = config.getString("hyperion.aws.ec2.instance.type")
-  lazy val ec2ImageId = config.getString(s"hyperion.aws.ec2.image.$region")
+  lazy val ec2ImageId = config.getString(s"hyperion.aws.ec2.image.$ec2Region")
   lazy val ec2TerminateAfter = config.getString("hyperion.aws.ec2.terminate")
+
+  // EMR default configuration
+  lazy val emrRegion = config.getString("hyperion.aws.emr.region")
+  lazy val emrKeyPair = Try(config.getString("hyperion.aws.emr.keypair")).toOption
+  lazy val emrAvailabilityZone = Try(config.getString("hyperion.aws.emr.availability_zone")).toOption
+  lazy val emrSubnetId = Try(config.getString("hyperion.aws.emr.subnet")).toOption
+  lazy val emrRole = config.getString("hyperion.aws.emr.role")
+  lazy val emrResourceRole = config.getString("hyperion.aws.emr.resource.role")
 
   lazy val emrAmiVersion = config.getString("hyperion.aws.emr.ami.version")
   lazy val emrInstanceType = config.getString("hyperion.aws.emr.instance.type")
