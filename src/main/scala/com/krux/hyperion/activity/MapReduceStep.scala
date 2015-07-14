3967f03d28d82cfb5a222c7d2353b49e3d7f9a72
Seth Yates <seth.yates@krux.com>
2015-07-14 15:00:54 +1000

    Switch to apply for SparkStep

--- src/main/scala/com/krux/hyperion/activity/MapReduceStep.scala
+++ src/main/scala/com/krux/hyperion/activity/MapReduceStep.scala
@@ -3,16 +3,27 @@ package com.krux.hyperion.activity
 /**
  * A MapReduce step that runs on MapReduce Cluster
  */
-case class MapReduceStep(
-  jar: Option[String] = None,
-  mainClass: Option[String] = None,
-  args: Seq[String] = Seq()
+case class MapReduceStep private(
+  jar: Option[String],
+  mainClass: Option[String],
+  args: Seq[String]
 ) {
 
   def withJar(jar: String) = this.copy(jar = Option(jar))
   def withMainClass(mainClass: String) = this.copy(mainClass = Option(mainClass.stripSuffix("$")))
   def withArguments(arg: String*) = this.copy(args = args ++ arg)
 
-  def toStepString = (jar.toSeq ++ mainClass.toSeq ++ args).mkString(",")
+  override def toString(): String = (jar.toSeq ++ mainClass.toSeq ++ args).mkString(",")
 
 }
+
+object MapReduceStep {
+
+  def apply(): MapReduceStep = MapReduceStep(
+    jar = None,
+    mainClass = None,
+    args = Seq()
+  )
+
+}
+
--- src/main/scala/com/krux/hyperion/activity/SparkStep.scala
+++ src/main/scala/com/krux/hyperion/activity/SparkStep.scala
@@ -5,19 +5,31 @@ import com.krux.hyperion.HyperionContext
 /**
  * A spark step that runs on Spark Cluster
  */
-case class SparkStep(
-  jar: Option[String] = None,
-  mainClass: Option[String] = None,
-  args: Seq[String] = Seq()
-)(implicit hc: HyperionContext) {
-
-  val scriptRunner = Option("s3://elasticmapreduce/libs/script-runner/script-runner.jar")
-  val jobRunner = Option(s"${hc.scriptUri}run-spark-step.sh")
+case class SparkStep private(
+  jar: Option[String],
+  mainClass: Option[String],
+  args: Seq[String],
+  scriptRunner: Option[String],
+  jobRunner: Option[String]
+) {
 
   def withJar(jar: String) = this.copy(jar = Option(jar))
   def withMainClass(mainClass: String) = this.copy(mainClass = Option(mainClass.stripSuffix("$")))
   def withArguments(arg: String*) = this.copy(args = args ++ arg)
 
-  def toStepString = (scriptRunner.toSeq ++ jobRunner.toSeq ++ jar.toSeq ++ mainClass.toSeq ++ args).mkString(",")
+  override def toString(): String = (scriptRunner.toSeq ++ jobRunner.toSeq ++ jar.toSeq ++ mainClass.toSeq ++ args).mkString(",")
 
 }
+
+object SparkStep {
+
+  def apply()(implicit hc: HyperionContext): SparkStep = SparkStep(
+    jar = None,
+    mainClass = None,
+    args = Seq(),
+    scriptRunner = Option("s3://elasticmapreduce/libs/script-runner/script-runner.jar"),
+    jobRunner = Option(s"${hc.scriptUri}run-spark-step.sh")
+  )
+
+}
+
