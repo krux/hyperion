9d7a20afac6dbd8cbace5d470fb513b45de9d113
Seth Yates <seth.yates@krux.com>
2015-11-09 10:18:53 +1100

    Added setters

--- core/src/main/scala/com/krux/hyperion/common/HttpProxy.scala
+++ core/src/main/scala/com/krux/hyperion/common/HttpProxy.scala
@@ -1,24 +1,35 @@
 package com.krux.hyperion.common
 
+import com.krux.hyperion.parameter.Parameter
 import com.krux.hyperion.aws.{AdpRef, AdpHttpProxy}
 
 case class HttpProxy private (
   id: PipelineObjectId,
   hostname: Option[String],
-  port: Option[String],
+  port: Option[Parameter[Int]],
   username: Option[String],
-  password: Option[String],
+  password: Option[Parameter[String]],
   windowsDomain: Option[String],
   windowsWorkGroup: Option[String]
 ) extends PipelineObject {
+
+  def withHostname(hostname: String) = this.copy(hostname = Option(hostname))
+  def withPort(port: Parameter[Int]) = this.copy(port = Option(port))
+  def withUsername(username: String) = this.copy(username = Option(username))
+  def withPassword(password: Parameter[String]) = {
+    assert(password.isEncrypted)
+    this.copy(password = Option(password))
+  }
+  def withWindowsDomain(windowsDomain: String) = this.copy(windowsDomain = Option(windowsDomain))
+  def withWindowsWorkGroup(windowsWorkGroup: String) = this.copy(windowsWorkGroup = Option(windowsWorkGroup))
 
   lazy val serialize = AdpHttpProxy(
     id = id,
     name = id.toOption,
     hostname = hostname,
-    port = port,
+    port = port.map(_.toString),
     username = username,
-    `*password` = password,
+    `*password` = password.map(_.toString),
     windowsDomain = windowsDomain,
     windowsWorkGroup = windowsWorkGroup
   )
@@ -30,10 +41,10 @@ case class HttpProxy private (
 }
 
 object HttpProxy {
-  def apply(): HttpProxy = HttpProxy(
+  def apply(host: String, port: Parameter[Int]): HttpProxy = HttpProxy(
     id = PipelineObjectId(HttpProxy.getClass),
-    hostname = None,
-    port = None,
+    hostname = Option(host),
+    port = Option(port),
     username = None,
     password = None,
     windowsDomain = None,
