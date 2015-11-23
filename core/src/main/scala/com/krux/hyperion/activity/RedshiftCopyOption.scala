c287c127ce777e22ff87f110dec2f711321cdc3a
Kexin Xie <kxie@krux.com>
2015-11-23 12:13:52 -0800

    (fixes #273) the rest of Data Conversion Parameters to redshift copy options

--- core/src/main/scala/com/krux/hyperion/activity/RedshiftCopyOption.scala
+++ core/src/main/scala/com/krux/hyperion/activity/RedshiftCopyOption.scala
@@ -34,4 +34,67 @@ object RedshiftCopyOption {
     def repr = Seq("MAXERROR", errorCount.toString)
   }
 
+  def acceptInvChars = new RedshiftCopyOption {
+    def repr = Seq("ACCEPTINVCHARS")
+  }
+
+  def acceptInvChars(replacementChar: Char) = new RedshiftCopyOption {
+    def repr = Seq("ACCEPTINVCHARS", s"'$replacementChar'")
+  }
+
+  def acceptAnyDate = new RedshiftCopyOption {
+    def repr = Seq("ACCEPTANYDATE")
+  }
+
+  def blanksAsNull = new RedshiftCopyOption {
+    def repr = Seq("BLANKSASNULL")
+  }
+
+  def dateFormat = new RedshiftCopyOption {
+    def repr = Seq("DATEFORMAT")
+  }
+
+  def dateFormat(dateFormatString: String) = new RedshiftCopyOption {
+    def repr = Seq("DATEFORMAT", s"'$dateFormatString'")
+  }
+
+  def encoding(fileEncoding: String) = new RedshiftCopyOption {
+    def repr = Seq("ENCODING", fileEncoding)
+  }
+
+  def explicitIds = new RedshiftCopyOption {
+    def repr = Seq("EXPLICIT_IDS")
+  }
+
+  def fillRecord = new RedshiftCopyOption {
+    def repr = Seq("FILLRECORD")
+  }
+
+  def ignoreBlankLines = new RedshiftCopyOption {
+    def repr = Seq("IGNOREBLANKLINES")
+  }
+
+  def ignoreHeader(numberRows: Long) = new RedshiftCopyOption {
+    def repr = Seq("IGNOREHEADER", numberRows.toString)
+  }
+
+  def removeQuotes = new RedshiftCopyOption {
+    def repr = Seq("REMOVEQUOTES")
+  }
+
+  def roundec = new RedshiftCopyOption {
+    def repr = Seq("ROUNDEC")
+  }
+
+  def timeFormat(format: String) = new RedshiftCopyOption {
+    def repr = Seq("TIMEFORMAT", s"'$format'")
+  }
+
+  def trimBlanks = new RedshiftCopyOption {
+    def repr = Seq("TRIMBLANKS")
+  }
+
+  def truncateColumns = new RedshiftCopyOption {
+    def repr = Seq("TRUNCATECOLUMNS")
+  }
 }
