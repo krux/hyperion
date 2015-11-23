42ceb9cc3f02a285162aa81a80cee695f22c0dbd
Kexin Xie <kxie@krux.com>
2015-11-23 14:41:50 -0800

    chagne redshift copy option to case class

--- core/src/main/scala/com/krux/hyperion/activity/RedshiftCopyOption.scala
+++ core/src/main/scala/com/krux/hyperion/activity/RedshiftCopyOption.scala
@@ -1,100 +1,53 @@
 package com.krux.hyperion.activity
 
-trait RedshiftCopyOption {
-  def repr: Seq[String]
-}
+case class RedshiftCopyOption(repr: Seq[String])
 
 object RedshiftCopyOption {
 
-  def csv(quote: String) = new RedshiftCopyOption {
-    def repr = Seq("CSV", "QUOTE", quote)
-  }
+  def csv(quote: String) = RedshiftCopyOption(Seq("CSV", "QUOTE", quote))
 
-  def csv = new RedshiftCopyOption {
-    def repr = Seq("CSV")
-  }
+  def csv = RedshiftCopyOption(Seq("CSV"))
 
-  def gzip = new RedshiftCopyOption {
-    def repr = Seq("GZIP")
-  }
+  def gzip = RedshiftCopyOption(Seq("GZIP"))
 
-  def delimiter(delChar: String) = new RedshiftCopyOption {
-    def repr = Seq("DELIMITER", s"'$delChar'")
-  }
+  def delimiter(delChar: String) = RedshiftCopyOption(Seq("DELIMITER", s"'$delChar'"))
 
-  def escape = new RedshiftCopyOption {
-    def repr = Seq("ESCAPE")
-  }
+  def escape = RedshiftCopyOption(Seq("ESCAPE"))
 
-  def nullAs(nullStr: String) = new RedshiftCopyOption {
-    def repr = Seq("NULL", s"'$nullStr'")
-  }
+  def nullAs(nullStr: String) = RedshiftCopyOption(Seq("NULL", s"'$nullStr'"))
 
-  def maxError(errorCount: Int) = new RedshiftCopyOption {
-    def repr = Seq("MAXERROR", errorCount.toString)
-  }
+  def maxError(errorCount: Int) = RedshiftCopyOption(Seq("MAXERROR", errorCount.toString))
 
-  def acceptInvChars = new RedshiftCopyOption {
-    def repr = Seq("ACCEPTINVCHARS")
-  }
+  def acceptInvChars = RedshiftCopyOption(Seq("ACCEPTINVCHARS"))
 
-  def acceptInvChars(replacementChar: Char) = new RedshiftCopyOption {
-    def repr = Seq("ACCEPTINVCHARS", s"'$replacementChar'")
-  }
+  def acceptInvChars(replacementChar: Char) =
+    RedshiftCopyOption(Seq("ACCEPTINVCHARS", s"'$replacementChar'"))
 
-  def acceptAnyDate = new RedshiftCopyOption {
-    def repr = Seq("ACCEPTANYDATE")
-  }
+  def acceptAnyDate = RedshiftCopyOption(Seq("ACCEPTANYDATE"))
 
-  def blanksAsNull = new RedshiftCopyOption {
-    def repr = Seq("BLANKSASNULL")
-  }
+  def blanksAsNull = RedshiftCopyOption(Seq("BLANKSASNULL"))
 
-  def dateFormat = new RedshiftCopyOption {
-    def repr = Seq("DATEFORMAT")
-  }
+  def dateFormat = RedshiftCopyOption(Seq("DATEFORMAT"))
 
-  def dateFormat(dateFormatString: String) = new RedshiftCopyOption {
-    def repr = Seq("DATEFORMAT", s"'$dateFormatString'")
-  }
+  def dateFormat(dateFormatString: String) = RedshiftCopyOption(Seq("DATEFORMAT", s"'$dateFormatString'"))
 
-  def encoding(fileEncoding: String) = new RedshiftCopyOption {
-    def repr = Seq("ENCODING", fileEncoding)
-  }
+  def encoding(fileEncoding: String) = RedshiftCopyOption(Seq("ENCODING", fileEncoding))
 
-  def explicitIds = new RedshiftCopyOption {
-    def repr = Seq("EXPLICIT_IDS")
-  }
+  def explicitIds = RedshiftCopyOption(Seq("EXPLICIT_IDS"))
 
-  def fillRecord = new RedshiftCopyOption {
-    def repr = Seq("FILLRECORD")
-  }
+  def fillRecord = RedshiftCopyOption(Seq("FILLRECORD"))
 
-  def ignoreBlankLines = new RedshiftCopyOption {
-    def repr = Seq("IGNOREBLANKLINES")
-  }
+  def ignoreBlankLines = RedshiftCopyOption(Seq("IGNOREBLANKLINES"))
 
-  def ignoreHeader(numberRows: Long) = new RedshiftCopyOption {
-    def repr = Seq("IGNOREHEADER", numberRows.toString)
-  }
+  def ignoreHeader(numberRows: Long) = RedshiftCopyOption(Seq("IGNOREHEADER", numberRows.toString))
 
-  def removeQuotes = new RedshiftCopyOption {
-    def repr = Seq("REMOVEQUOTES")
-  }
+  def removeQuotes = RedshiftCopyOption(Seq("REMOVEQUOTES"))
 
-  def roundec = new RedshiftCopyOption {
-    def repr = Seq("ROUNDEC")
-  }
+  def roundec = RedshiftCopyOption(Seq("ROUNDEC"))
 
-  def timeFormat(format: String) = new RedshiftCopyOption {
-    def repr = Seq("TIMEFORMAT", s"'$format'")
-  }
+  def timeFormat(format: String) = RedshiftCopyOption(Seq("TIMEFORMAT", s"'$format'"))
 
-  def trimBlanks = new RedshiftCopyOption {
-    def repr = Seq("TRIMBLANKS")
-  }
+  def trimBlanks = RedshiftCopyOption(Seq("TRIMBLANKS"))
 
-  def truncateColumns = new RedshiftCopyOption {
-    def repr = Seq("TRUNCATECOLUMNS")
-  }
+  def truncateColumns = RedshiftCopyOption(Seq("TRUNCATECOLUMNS"))
 }
