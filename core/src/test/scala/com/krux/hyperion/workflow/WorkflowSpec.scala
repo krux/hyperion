f870fda5f64d3c1a268f0c06c15a31fa2d085abb
bakerlee <lee@leebaker.com>
2016-05-20 15:11:16 -0500

    (fixes #388) Make hyperion.log.uri optional

---
+++ core/src/test/scala/com/krux/hyperion/workflow/WorkflowSpec.scala
@@ -0,0 +1,20 @@
+package com.krux.hyperion.workflow
+
+import com.krux.hyperion.DataPipelineDef
+import com.krux.hyperion.DataPipelineDef._
+import com.krux.hyperion.Schedule.onceAtActivation
+import com.krux.hyperion.activity.Script.string2Script
+import com.krux.hyperion.activity.ShellCommandActivity
+import com.krux.hyperion.resource.WorkerGroup
+import org.scalatest.FlatSpec
+
+class WorkflowSpec extends FlatSpec {
+  it should "not require logUri" in {
+    object NoConfigWorkflow extends DataPipelineDef {
+      override def schedule = onceAtActivation
+      override def workflow = ShellCommandActivity("hello world")(WorkerGroup("foo"))
+    }
+
+    dataPipelineDef2Json(NoConfigWorkflow)
+  }
+}
