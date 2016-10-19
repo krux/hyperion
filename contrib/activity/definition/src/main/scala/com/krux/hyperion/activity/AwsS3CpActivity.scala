09fb7b4a1fcc0cb8ac2cc1b38eba881d21a40738
Pulkit Khetrapal <pkhetrapal@krux.com>
2016-10-18 17:24:25 -0700

    adds aws cp with profile activity

---
+++ contrib/activity/definition/src/main/scala/com/krux/hyperion/activity/AwsS3CpActivity.scala
@@ -0,0 +1,46 @@
+package com.krux.hyperion.activity
+
+import com.krux.hyperion.adt.{HS3Uri, HString}
+import com.krux.hyperion.common.{BaseFields, PipelineObjectId}
+import com.krux.hyperion.resource.{Ec2Resource, Resource}
+import com.krux.hyperion.expression.RunnableObject
+
+case class AwsS3CpActivity private(
+                                    baseFields: BaseFields,
+                                    activityFields: ActivityFields[Ec2Resource],
+                                    shellCommandActivityFields: ShellCommandActivityFields,
+                                    sourceS3Path: HS3Uri,
+                                    destinationS3Path: HS3Uri
+                                  ) extends BaseShellCommandActivity {
+
+  type Self = AwsS3CpActivity
+
+  def updateBaseFields(fields: BaseFields) = copy(baseFields = fields)
+  def updateActivityFields(fields: ActivityFields[Ec2Resource]) = copy(activityFields = fields)
+  def updateShellCommandActivityFields(fields: ShellCommandActivityFields) = copy(shellCommandActivityFields = fields)
+
+  def useProfile(profile: HString) = updateShellCommandActivityFields(
+    shellCommandActivityFields.copy(script =
+      s"""`aws s3 cp s3://krux-profiles/credentials ~/.aws/credentials`;
+          `aws s3 cp s3://krux-profiles/config ~/.aws/config`;
+          `aws s3 cp --recursive --profile $profile $sourceS3Path $destinationS3Path`;
+        """
+    )
+  )
+}
+
+object AwsS3CpActivity extends RunnableObject {
+
+  def apply(
+             sourceS3Path: HS3Uri,
+             destinationS3Path: HS3Uri
+           )(runsOn: Resource[Ec2Resource]): AwsS3CpActivity =
+
+    new AwsS3CpActivity(
+      baseFields = BaseFields(PipelineObjectId(AwsS3CpActivity.getClass)),
+      activityFields = ActivityFields(runsOn),
+      shellCommandActivityFields = ShellCommandActivityFields(s"aws s3 cp --recursive $sourceS3Path $destinationS3Path"),
+      sourceS3Path = sourceS3Path,
+      destinationS3Path = destinationS3Path
+    )
+}
