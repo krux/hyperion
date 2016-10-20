fb7c7c2adc8f3ca7dfd7eecb12c2f757de15ec2a
Pulkit Khetrapal <pkhetrapal@krux.com>
2016-10-19 17:16:15 -0700

    addresses comments

--- contrib/activity/definition/src/main/scala/com/krux/hyperion/activity/AwsS3CpActivity.scala
+++ contrib/activity/definition/src/main/scala/com/krux/hyperion/activity/AwsS3CpActivity.scala
@@ -1,17 +1,20 @@
 package com.krux.hyperion.activity
 
-import com.krux.hyperion.adt.{HS3Uri, HString}
+import com.krux.hyperion.adt.{HBoolean, HS3Uri, HString, HType}
 import com.krux.hyperion.common.{BaseFields, PipelineObjectId}
 import com.krux.hyperion.resource.{Ec2Resource, Resource}
 import com.krux.hyperion.expression.RunnableObject
 
 case class AwsS3CpActivity private(
-                                    baseFields: BaseFields,
-                                    activityFields: ActivityFields[Ec2Resource],
-                                    shellCommandActivityFields: ShellCommandActivityFields,
-                                    sourceS3Path: HS3Uri,
-                                    destinationS3Path: HS3Uri
-                                  ) extends BaseShellCommandActivity {
+  baseFields: BaseFields,
+  activityFields: ActivityFields[Ec2Resource],
+  shellCommandActivityFields: ShellCommandActivityFields,
+  sourceS3Path: HS3Uri,
+  destinationS3Path: HS3Uri,
+  recursive: HBoolean,
+  profile: Option[HString],
+  credentialsSource: Option[HString]
+) extends BaseShellCommandActivity {
 
   type Self = AwsS3CpActivity
 
@@ -19,14 +22,21 @@ case class AwsS3CpActivity private(
   def updateActivityFields(fields: ActivityFields[Ec2Resource]) = copy(activityFields = fields)
   def updateShellCommandActivityFields(fields: ShellCommandActivityFields) = copy(shellCommandActivityFields = fields)
 
-  def useProfile(
-      profile: HString,
-      credentialsS3Uri: HS3Uri,
-      configS3Uri: HS3Uri) = updateShellCommandActivityFields(
+  def isRecursive() = copy(recursive = HBoolean.True)
+  def withProfile(profile: HString) = copy(profile = Option(profile))
+  def useCredentialsSource(credentialsSource: HString) = copy(credentialsSource = Option(credentialsSource))
+
+  def build() = updateShellCommandActivityFields(
     shellCommandActivityFields.copy(script =
-      s"""`aws s3 cp $credentialsS3Uri ~/.aws/credentials`;
-          `aws s3 cp $configS3Uri ~/.aws/config`;
-          `aws s3 cp --recursive --profile $profile $sourceS3Path $destinationS3Path`;
+      s"""ARGS=""
+          if ${!credentialsSource.isEmpty} ; then
+            aws s3 sync ${credentialsSource.getOrElse("")} ~/.aws/;
+            ARGS="$$ARGS --profile ${profile.getOrElse("default")}";
+          fi
+          if $recursive ; then
+            ARGS="$$ARGS --recursive";
+          fi
+          aws s3 cp $$ARGS $sourceS3Path $destinationS3Path;
         """
     )
   )
@@ -42,8 +52,11 @@ object AwsS3CpActivity extends RunnableObject {
     new AwsS3CpActivity(
       baseFields = BaseFields(PipelineObjectId(AwsS3CpActivity.getClass)),
       activityFields = ActivityFields(runsOn),
-      shellCommandActivityFields = ShellCommandActivityFields(s"aws s3 cp --recursive $sourceS3Path $destinationS3Path"),
+      shellCommandActivityFields = ShellCommandActivityFields(s"aws s3 cp $sourceS3Path $destinationS3Path"),
       sourceS3Path = sourceS3Path,
-      destinationS3Path = destinationS3Path
+      destinationS3Path = destinationS3Path,
+      recursive = HBoolean.False,
+      profile = None,
+      credentialsSource = None
     )
 }
