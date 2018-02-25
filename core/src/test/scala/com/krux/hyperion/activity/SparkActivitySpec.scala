380c0a11c6bcd98a61179ef662953920fdb6da7c
Kexin Xie <ikexinxie@gmail.com>
2018-02-24 21:28:46 -0800

    :construction: fix more test cases

--- core/src/test/scala/com/krux/hyperion/activity/SparkActivitySpec.scala
+++ core/src/test/scala/com/krux/hyperion/activity/SparkActivitySpec.scala
@@ -8,23 +8,15 @@ import com.typesafe.config.ConfigFactory
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers._
 
+@deprecated("Use EmrActivity with SparkStep instead", "5.0.0")
 class SparkActivitySpec extends FlatSpec {
   implicit val hc = new HyperionContext(ConfigFactory.load("example"))
 
   class MainClass
 
-  it should "handle EMR release 4.x.x" in {
-    val cluster = SparkCluster().withReleaseLabel("emr-4.6.0")
-    val testStep = SparkStep(S3Uri("s3://something.jar")).withMainClass(MainClass)
-    val activity = SparkActivity(cluster).withSteps(testStep)
-    activity.steps.length shouldBe 1
-    activity.steps.count(_.jobRunner.toString.contains("run-spark-step-release-label.sh")) shouldEqual 1
-    activity.steps.count(_.scriptRunner.toString.contains("script-runner.jar")) shouldEqual 1
-  }
-
   it should "be backwards compatible" in {
     val cluster = SparkCluster()
-    val testStep = SparkStep(S3Uri("s3://something.jar")).withMainClass(MainClass)
+    val testStep = LegacySparkStep(S3Uri("s3://something.jar")).withMainClass(MainClass)
     val activity = SparkActivity(cluster).withSteps(testStep)
     activity.steps.length shouldBe 1
     activity.steps.count(_.scriptRunner.toString.contains("s3://elasticmapreduce/libs/script-runner/script-runner.jar")) shouldEqual 1
