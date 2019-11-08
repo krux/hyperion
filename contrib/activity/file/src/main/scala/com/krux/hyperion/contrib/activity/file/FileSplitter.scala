113e7797f8f7f23c4d4c5523de8702f118e7d6d7
Giridhar Bhageshpur <g.bhageshpur@gbhageshpu-ltm1.internal.salesforce.com>
2019-11-07 21:20:18 -0800

    adding spaces and removing compressiontypeending

--- contrib/activity/file/src/main/scala/com/krux/hyperion/contrib/activity/file/FileSplitter.scala
+++ contrib/activity/file/src/main/scala/com/krux/hyperion/contrib/activity/file/FileSplitter.scala
@@ -20,8 +20,8 @@ class FileSplitter(
   compressionFormat: CompressionFormat = CompressionFormat.GZ
 ) {
   private class FileState(
-   val outputStreamWriter: Option[OutputStream] = None
- ) {
+    val outputStreamWriter: Option[OutputStream] = None
+  ) {
     var numberOfLines: Long = 0L
     var numberOfBytes: Long = 0L
 
@@ -52,13 +52,11 @@ class FileSplitter(
       else s
     }, bufferSize.toInt)
     var needFile = true
-    val sourceName = source.getName
-    val compressionTypeEnding = sourceName.substring(sourceName.lastIndexOf(".")+1, sourceName.length)
 
     var read = input.read()
     while (read != -1) {
       if (needFile) {
-        val split = startNewFile(compressionTypeEnding)
+        val split = startNewFile()
         splits += split
 
         println(s"Creating split #${splits.size}: ${split.getAbsolutePath}")
@@ -81,7 +79,7 @@ class FileSplitter(
     fileState = new FileState
   }
 
-  private def startNewFile(compressionTypeEnding: String): File = {
+  private def startNewFile(): File = {
     fileState.close()
 
     val file = File.createTempFile("split-", ".tmp", temporaryDirectory)
